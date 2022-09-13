c0b2b3163365e20e18fbeb002946fa98a3175694
SaurabhPagrut <saurabhpagrut2016@gmail.com>
2022-09-13 19:07:40 +0530

    added github portfolio

---
+++ Components/navbar.js
@@ -0,0 +1,16 @@
+var navbarjs = () => {
+    return `
+        <div id="logo">
+        <a href="./index.html"><img src="./Images/Personal/Saurabh.svg" alt="SP"></a>
+        </div>
+        <div class="sections">
+            <a class="ac" href="#home">HOME</a>
+            <a class="ac" href="#about">ABOUT</a>
+            <a class="ac" href="#skills">SKILLS</a>
+            <a class="ac" href="#projects">PROJECTS</a>
+            <a class="ac" href="#contact_me">CONTACT</a>
+        </div>
+    `;
+};
+
+export { navbarjs };
---
+++ JS/index.js
@@ -0,0 +1,37 @@
+import { navbarjs } from '../Components/navbar.js';
+import { homejs } from "../Components/home.js";
+import { aboutjs } from "../Components/about.js";
+import { skillsjs } from "../Components/skills.js";
+import { projectsjs } from "../Components/projects.js";
+import { contactjs } from "../Components/contact.js";
+
+var navbardiv = document.querySelector(".navbar, .navbarchange");
+navbardiv.innerHTML = navbarjs();
+
+let home = document.getElementById("home");
+home.innerHTML = homejs();
+
+let about = document.getElementById("about");
+about.innerHTML = aboutjs();
+
+let skills = document.getElementById("skills");
+skills.innerHTML = skillsjs();
+
+let projects = document.getElementById("projects");
+projects.innerHTML = projectsjs();
+
+let contact = document.getElementById("contact_me");
+contact.innerHTML = contactjs();
+
+$(document).ready(function () {
+    $(window).scroll(function () {
+        let navdiv = $(document).scrollTop();
+        if ((navdiv > 660 && navdiv < 1410) || (navdiv > 2110 && navdiv < 4750)) {
+            $(".navbar").addClass("navbarchange");
+            $(".ac").addClass("achange");
+        } else {
+            $(".navbar").removeClass("navbarchange");
+            $(".ac").removeClass("achange");
+        }
+    });
+});
